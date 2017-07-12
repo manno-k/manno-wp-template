209a72ef626e0f940852cf059997a1889459e8b7
manno-k <art.kuromi@gmail.com>
2017-07-12 16:26:17 +0900

    bootstrap追加

--- functions.php
+++ functions.php
@@ -9,3 +9,9 @@ function my_scripts_method() {
 	wp_enqueue_script('bootstrap',get_bloginfo('stylesheet_directory').'/src/js/bootstrap.js', array(), '1.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+
+// nav_menu aタグに任意のclassを追加
+add_filter('walker_nav_menu_start_el', 'add_class_on_link', 10, 4);
+ function add_class_on_link($item_output, $item){
+ return preg_replace('/(<a.*?)/', '$1' . " class='nav-link'", $item_output);
+}
