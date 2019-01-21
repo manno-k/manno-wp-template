6114b2bfd5110330fc813b82f62ae37aa494a982
manno-k <art.kuromi@gmail.com>
2019-01-21 19:54:28 +0900

    画像の差分だけを圧縮するように変更

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,22 @@
 // ファイル読み込み
 //---------------------------------------------------------------------------
 var
-	gulp = require('gulp'),
-	gulpLoadPlugins = require('gulp-load-plugins'),
-	$ = gulpLoadPlugins(),
-	// Configファイルを読みます。
-	config = require('./gulpconfig'),
-	sass = require('gulp-sass'),
-	postcss = require('gulp-postcss'),
-	cssnext = require('postcss-cssnext'),
-	browserSync = require('browser-sync'),
-	runSequence = require('run-sequence'),
-	template = require('gulp-template'),
-	stylelint = require('stylelint'),
-	syntax_scss = require('postcss-scss'),
-	reporter = require('postcss-reporter'),
-	sourcemaps = require('gulp-sourcemaps')
+  gulp = require('gulp'),
+  gulpLoadPlugins = require('gulp-load-plugins'),
+  $ = gulpLoadPlugins(),
+  // Configファイルを読みます。
+  config = require('./gulpconfig'),
+  sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  cssnext = require('postcss-cssnext'),
+  browserSync = require('browser-sync'),
+  runSequence = require('run-sequence'),
+  template = require('gulp-template'),
+  stylelint = require('stylelint'),
+  syntax_scss = require('postcss-scss'),
+  reporter = require('postcss-reporter'),
+  sourcemaps = require('gulp-sourcemaps'),
+  changed = require("gulp-changed");
 ;
 
 
@@ -27,42 +28,42 @@ var
 
 gulp.task('lint:styles', function () {
 
-	return gulp.src(config.source.css, {since: gulp.lastRun('lint:styles')})
-	.pipe(postcss([
-		stylelint(),
-		reporter({clearMessages: true})
-	], {syntax: syntax_scss}));
+  return gulp.src(config.source.css, {since: gulp.lastRun('lint:styles')})
+  .pipe(postcss([
+    stylelint(),
+    reporter({clearMessages: true})
+  ], {syntax: syntax_scss}));
 });
 
 // make styles
 gulp.task('make:styles', function () {
-	processors = [
-		cssnext({
-			browsers: config.styles.prefix,
-			features: config.styles.features,
-		}),
-	];
-	return gulp
-	.src(config.source.css)
-	.pipe(
-		$.plumber({
-			errorHandler: function (err) {
-				console.log(err.messageFormatted);
-				this.emit('end');
-			}
-		})
-	)
-	.pipe(sass())
-	.pipe(sass().on('error', sass.logError))
-	.pipe(postcss(processors))
-	.pipe($.cssmin())
-	.pipe(gulp.dest(config.build.css))
-	.pipe(browserSync.stream());
+  processors = [
+    cssnext({
+      browsers: config.styles.prefix,
+      features: config.styles.features,
+    }),
+  ];
+  return gulp
+  .src(config.source.css)
+  .pipe(
+    $.plumber({
+      errorHandler: function (err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    })
+  )
+  .pipe(sass())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss(processors))
+  .pipe($.cssmin())
+  .pipe(gulp.dest(config.build.css))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('styles', gulp.series(
-	'lint:styles',
-	'make:styles'
+  'lint:styles',
+  'make:styles'
 ));
 
 
@@ -72,31 +73,31 @@ gulp.task('styles', gulp.series(
 
 // scriptを一つにまとめるタスク。
 gulp.task('concat', function () {
-	return gulp
-	.src(config.script.concat)
-	.pipe($.plumber())
-	.pipe($.concat(config.script.filename))
-	.pipe($.uglify())
-	.pipe(
-		$.rename({
-			extname: '.min.js'
-		})
-	)
-	.pipe(gulp.dest(config.build.js));
+  return gulp
+  .src(config.script.concat)
+  .pipe($.plumber())
+  .pipe($.concat(config.script.filename))
+  .pipe($.uglify())
+  .pipe(
+    $.rename({
+      extname: '.min.js'
+    })
+  )
+  .pipe(gulp.dest(config.build.js));
 });
 
 // 全jsファイルを圧縮するタスク
 gulp.task('uglify', function () {
-	return (eachscript = gulp
-	.src(config.source.js)
-	.pipe($.plumber())
-	.pipe($.uglify())
-	.pipe(
-		$.rename({
-			extname: '.min.js'
-		})
-	)
-	.pipe(gulp.dest(config.build.js)));
+  return (eachscript = gulp
+  .src(config.source.js)
+  .pipe($.plumber())
+  .pipe($.uglify())
+  .pipe(
+    $.rename({
+      extname: '.min.js'
+    })
+  )
+  .pipe(gulp.dest(config.build.js)));
 });
 
 gulp.task('script', gulp.parallel('concat', 'uglify'));
@@ -106,40 +107,43 @@ gulp.task('script', gulp.parallel('concat', 'uglify'));
 //---------------------------------------------------------------------------
 
 gulp.task('image-opt', function () {
-	return gulp.src(config.source.img)
-	.pipe($.plumber())
-	.pipe(
-		$.image({
-			pngquant: true,
-			optipng: true,
-			zopflipng: false,
-			advpng: true,
-			jpegRecompress: false,
-			jpegoptim: false,
-			mozjpeg: true,
-			gifsicle: true,
-			svgo: false,
-			concurrent: 10
-		})
-	)
-	.pipe(gulp.dest(config.build.img));
+  return gulp.src(config.source.img)
+  .pipe($.plumber())
+  .pipe(changed(config.build.img))
+  .pipe(
+    $.image({
+      pngquant: true,
+      optipng: true,
+      zopflipng: false,
+      advpng: true,
+      jpegRecompress: false,
+      jpegoptim: false,
+      mozjpeg: true,
+      gifsicle: true,
+      svgo: false,
+      concurrent: 10
+    })
+  )
+  .pipe(gulp.dest(config.build.img));
 });
 
 gulp.task('svgmin', function () {
-	return gulp
-	.src(config.source.img + '**/*.svg')
-	.pipe($.svgmin())
-	.pipe(gulp.dest(config.build.img));
+  return gulp
+  .src(config.source.img + '**/*.svg')
+  .pipe($.svgmin())
+  .pipe(gulp.dest(config.build.img));
 });
+
+gulp.task('image', gulp.series(gulp.parallel('image-opt', 'svgmin')));
 
 //---------------------------------------------------------------------------
 // BROWSER SYNC
 //---------------------------------------------------------------------------
 
 gulp.task('server', function () {
-	return browserSync.init(
-		config.server
-	);
+  return browserSync.init(
+    config.server
+  );
 });
 
 //---------------------------------------------------------------------------
@@ -147,41 +151,57 @@ gulp.task('server', function () {
 //---------------------------------------------------------------------------
 
 gulp.task('watch:styles', function () {
-	gulp.watch(
-		config.source.css,
-		gulp.series('styles')
-	);
+  gulp.watch(
+    config.source.css,
+    gulp.series('styles')
+  );
 });
 
 gulp.task('watch:scripts', function () {
-	// https://github.com/BrowserSync/browser-sync/issues/711
-	function reload (done) {
-		browserSync.reload();
-		done();
-	}
+  // https://github.com/BrowserSync/browser-sync/issues/711
+  function reload (done) {
+    browserSync.reload();
+    done();
+  }
 
-	gulp.watch(
-		config.source.js,
-		gulp.series('script', reload)
-	);
+  gulp.watch(
+    config.source.js,
+    gulp.series('script', reload)
+  );
 });
 
 gulp.task('watch:code', function () {
 
-	// https://github.com/BrowserSync/browser-sync/issues/711
-	function reload (done) {
-		browserSync.reload();
-		done();
-	}
+  // https://github.com/BrowserSync/browser-sync/issues/711
+  function reload (done) {
+    browserSync.reload();
+    done();
+  }
 
-	gulp.watch(config.watch.code, gulp.series(reload));
+  gulp.watch(config.watch.code, gulp.series(reload));
+});
+
+
+gulp.task('watch:images', function () {
+
+  // https://github.com/BrowserSync/browser-sync/issues/711
+  function reload (done) {
+    browserSync.reload();
+    done();
+  }
+
+  gulp.watch(
+    config.source.img,
+    gulp.series('image', reload)
+  );
 });
 
 
 gulp.task('watch', gulp.parallel(
-	'watch:styles',
-	'watch:scripts',
-	'watch:code'
+  'watch:styles',
+  'watch:scripts',
+  'watch:code',
+  'watch:images'
 ));
 
 //---------------------------------------------------------------------------
@@ -190,7 +210,3 @@ gulp.task('watch', gulp.parallel(
 // default
 
 gulp.task('default', gulp.series(gulp.parallel('server', 'watch')));
-
-// image
-
-gulp.task('image', gulp.series(gulp.parallel('image-opt', 'svgmin')));
