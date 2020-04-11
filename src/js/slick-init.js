091195f2caee863f7df38c21f089592856180365
manno-k <art.kuromi@gmail.com>
2020-04-11 15:53:30 +0800

    slickのバグの修正

--- src/js/slick-init.js
+++ src/js/slick-init.js
@@ -1,3 +1,23 @@
+/*
+詳しい解説
+https://webdesignday.jp/inspiration/technique/jquery-js/3847/
+ */
 jQuery(function ($){
-
+  $('.js-case').slick({
+    slidesToShow:3,
+    responsive:[
+      {
+        breakpoint: 768,
+        settings:{
+          slidesToShow:3,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings:{
+          slidesToShow:1,
+        }
+      },
+    ]
+  });
 });
